8563ee3544870f4af5ceb559c4ce2e8268fec418
Buck Brady <bbrady145@gmail.com>
2023-07-23 06:36:35 +0000

    feat(universe-sync): enable ancestry sync

--- apps/universe-sync/src/ancestries/ancestries.service.ts
+++ apps/universe-sync/src/ancestries/ancestries.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq'
+import { Inject, Injectable, Logger } from '@nestjs/common'
+import { Job } from 'bullmq'
+import { EVEClient } from 'libs/esi'
+import { PrismaService } from 'libs/prisma.service'
 
 @Injectable()
-export class AncestriesService {}
+@Processor('universe-ancestries')
+export class AncestriesService extends WorkerHost {
+  private readonly logger = new Logger('AncestriesService')
+  @Inject(PrismaService) private prisma: PrismaService
+
+  async process(job: Job<any, any, string>): Promise<any> {
+    const client = new EVEClient()
+
+    const res = await client.universe.getUniverseAncestries({})
+
+    res.forEach(async (ancestry) => {
+      await this.prisma.ancestry.upsert({
+        where: { Id: ancestry.id },
+        update: {
+          BloodlineId: ancestry.bloodline_id,
+          Description: ancestry.description,
+          Name: ancestry.name,
+          ShortDescription: ancestry.short_description,
+          IconId: ancestry.icon_id
+        },
+        create: {
+          Id: ancestry.id,
+          BloodlineId: ancestry.bloodline_id,
+          Description: ancestry.description,
+          Name: ancestry.name,
+          ShortDescription: ancestry.short_description,
+          IconId: ancestry.icon_id
+        }
+      })
+    })
+  }
+
+  @OnWorkerEvent('completed')
+  onCompleted() {
+    this.logger.log('completed')
+  }
+  @OnWorkerEvent('failed')
+  onFailed() {
+    this.logger.log('failed')
+  }
+}
