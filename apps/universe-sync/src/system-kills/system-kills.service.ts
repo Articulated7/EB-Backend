3b3eb39e4c6572e4e7789951c9aab3da510ce86f
Buck Brady <bbrady145@gmail.com>
2023-07-30 18:44:06 -0600

    feat: enable system-kills sync

--- apps/universe-sync/src/system-kills/system-kills.service.ts
+++ apps/universe-sync/src/system-kills/system-kills.service.ts
@@ -2,13 +2,11 @@ import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq'
 import { Inject, Injectable, Logger } from '@nestjs/common'
 import { Job } from 'bullmq'
 import { EVEClient } from 'libs/esi'
-import { PrismaService } from 'libs/prisma.service'
 
 @Injectable()
 @Processor('universe-system-kills')
 export class SystemKillsService extends WorkerHost {
   private readonly logger = new Logger('SystemKillsService')
-  @Inject(PrismaService) private prisma: PrismaService
 
   async process(job: Job<any, any, string>): Promise<any> {
     try {
