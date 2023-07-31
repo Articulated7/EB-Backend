cb78e4b2cc5470b7823f4e56de22e276a764e7f7
Buck Brady <bbrady145@gmail.com>
2023-07-30 18:41:50 -0600

    feat: enable graphic sync

--- apps/universe-sync/src/graphics/graphics.service.ts
+++ apps/universe-sync/src/graphics/graphics.service.ts
@@ -2,16 +2,38 @@ import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq'
 import { Inject, Injectable, Logger } from '@nestjs/common'
 import { Job } from 'bullmq'
 import { EVEClient } from 'libs/esi'
-import { PrismaService } from 'libs/prisma.service'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Graphic } from 'libs/database'
 
 @Injectable()
 @Processor('universe-graphics')
 export class GraphicsService extends WorkerHost {
   private readonly logger = new Logger('GraphicsService')
-  @Inject(PrismaService) private prisma: PrismaService
+
+  @InjectRepository(Graphic)
+  graphicRepository: Repository<Graphic>
 
   async process(job: Job<any, any, string>): Promise<any> {
     try {
+      const client = new EVEClient()
+      const res = await client.universe.getUniverseGraphicsGraphicId({
+        graphicId: job.data.id
+      })
+
+      await this.graphicRepository.upsert(
+        {
+          graphicId: res.graphic_id,
+          collisionFile: res.collision_file,
+          graphicFile: res.graphic_file,
+          iconFolder: res.icon_folder,
+          sofDna: res.sof_dna,
+          sofFationName: res.sof_fation_name,
+          sofHullName: res.sof_hull_name,
+          sofRaceName: res.sof_race_name
+        },
+        ['graphicId']
+      )
     } catch (e) {
       this.logger.error(e)
       return { error: e }
