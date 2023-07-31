21a995dfc983b7a8a0cd977731a16ceff696b733
Buck Brady <bbrady145@gmail.com>
2023-07-30 18:44:25 -0600

    feat: enable stations sync

--- apps/universe-sync/src/stations/stations.service.ts
+++ apps/universe-sync/src/stations/stations.service.ts
@@ -2,13 +2,11 @@ import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq'
 import { Inject, Injectable, Logger } from '@nestjs/common'
 import { Job } from 'bullmq'
 import { EVEClient } from 'libs/esi'
-import { PrismaService } from 'libs/prisma.service'
 
 @Injectable()
 @Processor('universe-stations')
 export class StationsService extends WorkerHost {
   private readonly logger = new Logger('StationsService')
-  @Inject(PrismaService) private prisma: PrismaService
 
   async process(job: Job<any, any, string>): Promise<any> {
     try {
